bc2a6aaa69d3fbabc96f92c9f55baf6939093fbb
EliTE08 <memeinsta84@gmail.com>
2024-04-23 10:39:32 -0400

    Wave Spawner Added
    Wave spawner now can actually spawn enemies.

--- JameGam39/Assets/Scripts/WaveManager.cs
+++ JameGam39/Assets/Scripts/WaveManager.cs
@@ -18,10 +18,22 @@ public class WaveManager : MonoBehaviour
         _waveNumber++;
         var enemiesPerRoundFunc = Mathf.RoundToInt(3f + 1.15f * _waveNumber * (float) Math.Pow(Math.E, -_waveNumber / 500f) + 4.5f * (float) Math.Sin(_waveNumber * math.PI / 25f));
         _enemiesThisRound = enemiesPerRoundFunc;
+        for(int i = 0; i < _enemiesThisRound; i++){
+            SpawnEnemy();
+        }
     }
     
     public void EnemyKilled()
     {
         _enemiesKilled++;
+    }
+
+    public void SpawnEnemy()
+    {
+        int spawnIndex = Random.Range(0, spawnPositions.Count-1);
+        Vector2 enemyPosition = spawnPositions[spawnIndex].transform.position;
+        GameObject enemySpawn = enemies.GetRandomObject();
+        Instantiate(enemySpawn, enemyPosition, Quaternion.identity);
+
     }
 }
