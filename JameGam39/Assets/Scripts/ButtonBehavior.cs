74222e45112784926251625600a08234647c06f2
EliTE08 <memeinsta84@gmail.com>
2024-04-24 13:39:58 -0400

    Transitions Completed
    Transitions are now working and look good in my opinion.

--- JameGam39/Assets/Scripts/ButtonBehavior.cs
+++ JameGam39/Assets/Scripts/ButtonBehavior.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonBehavior : MonoBehaviour
 {
     [SerializeField] private string mainScene = "Main";
+    [SerializeField] private RectTransform fader;
     
-    public void LoadGame()
+
+    private void Start(){
+        fader.gameObject.SetActive(true);
+        fader.DOScale(new Vector2(1,1), 0f);
+        fader.DOScale(Vector2.zero, 1f).SetEase(Ease.InOutExpo).OnComplete (() => {
+            fader.gameObject.SetActive(false);
+        });
+    }
+
+    public void OpenGameScene()
     {
+        fader.gameObject.SetActive(true);
+        fader.DOScale(Vector2.zero, 0f);
+        fader.DOScale(new Vector2(1,1), 1f).SetEase(Ease.InOutExpo).OnComplete (() => {
+            Invoke("LoadScene", 0.5f);
+        });
+    }
+
+    public void LoadScene(){
         SceneManager.LoadScene(mainScene);
     }
     
